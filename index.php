d32fc67a3f4f946c272588b9a6276fbd6bdc7beb
Andhika Setiawan <andhikatrickster@gmail.com>
2021-12-04 22:23:07 +0700

    Add files via upload
    fixing

--- index.php
+++ index.php
@@ -10,6 +10,58 @@
     <title>Transaction</title>
 </head>
 <style>
+    html, #page { padding:0; margin:0;}
+        body { margin:0; padding:0; width:100%; color:#959595; font:normal 12px/2.0em Sans-Serif;} 
+        h1, h2, h3, h4, h5, h6 {color:darkblue;}
+        #page { background:#fff;}
+        #header, #footer, #top-nav, #content, #content #contentbar, #content #sidebar { margin:0; padding:0;}
+                    
+        /* Logo */
+        #logo { padding:0; width:auto; float:left;}
+        #logo h1 a, h1 a:hover { color:darkblue; text-decoration:none;}
+        #logo h1 span { color:#d3d3f9;}
+
+        /* Header */
+        #header { background:#fff; }
+        #header-inner { margin:0 auto; padding:0; width:970px;}
+        
+        /* Feature */
+        .feature { background:blue;padding:18px;}
+        .feature-inner { margin:auto;width:970px; }
+        .feature-inner h1 {color:#d3d3f9;font-size:32px;}
+        
+        /* Menu */
+        #top-nav { margin:0 auto; padding:0px 0 0; height:37px; float:right;}
+        #top-nav ul { list-style:none; padding:0; height:37px; float:left;}
+        #top-nav ul li { margin:0; padding:0 0 0 8px; float:left;}
+        #top-nav ul li a { display:block; margin:0; padding:8px 20px; color:blue; text-decoration:none;}
+        #top-nav ul li.active a, #top-nav ul li a:hover { color:#d3d3f9;}
+        
+        /* Content */
+        #content-inner { margin:0 auto; padding:10px 0; width:970px;background:#fff;}
+        #content #contentbar { margin:0; padding:0; float:right; width:760px;}
+        #content #contentbar .article { margin:0 0 24px; padding:0 20px 0 15px; }
+        #content #sidebar { padding:0; float:left; width:200px;}
+        #content #sidebar .widget { margin:0 0 12px; padding:8px 8px 8px 13px;line-height:1.4em;}
+        #content #sidebar .widget h3 a { text-decoration:none;}
+        #content #sidebar .widget ul { margin:0; padding:0; list-style:none; color:#959595;}
+        #content #sidebar .widget ul li { margin:0;}
+        #content #sidebar .widget ul li { padding:4px 0; width:185px;}
+        #content #sidebar .widget ul li a { color:blue; text-decoration:none; margin-left:-16px; padding:4px 8px 4px 16px;}
+        #content #sidebar .widget ul li a:hover { color:#d3d3f9; font-weight:bold; text-decoration:none;}
+        
+        /* Footerblurb */
+        #footerblurb { background:#d3d3f9;color:blue;}
+        #footerblurb-inner { margin:0 auto; width:922px; padding:24px;}
+        #footerblurb .column { margin:0; text-align:justify; float:left;width:250px;padding:0 24px;}
+        
+        /* Footer */
+        #footer { background:#fff;}
+        #footer-inner { margin:auto; text-align:center; padding:12px; width:922px;}
+        #footer a {color:blue;text-decoration:none;}
+        
+        /* Clear both sides to assist with div alignment  */
+        .clr { clear:both; padding:0; margin:0; width:100%; font-size:0px; line-height:0px;}
     .grid {
         display: grid;
         grid-template-columns: 200px auto;
@@ -104,23 +156,38 @@
     padding: 2px 8px;
     border-radius: 5px;
     }
+    .demoparagraph {
+    -webkit-column-count: 1;
+    -moz-column-count: 1;
+    column-count: 1;
+    -webkit-column-gap: 18px;
+    -moz-column-gap: 18px;
+    column-gap: 18px;
+    -webkit-column-rule: 1px solid rgba(28,110,164,0.37);
+    -moz-column-rule: 1px solid rgba(28,110,164,0.37);
+    column-rule: 1px solid rgba(28,110,164,0.37);
+    }
 </style>
 <body>
-   
-    <form id="trxForm" name="trxForm" method="post" action="trx.php">
-        <div>
-            <label for="trx">Amount </label>
-            <input type="number" id="trx" name="amount">
-        </div>
-        <div>
-            <label for="type">Type </label>
-            <select id="type" name="type">
-                <option value="C">Credit</option>
-                <option value="D">Debit</option>
-            </select>
-        </div>
-        <button type="button" id="submit" onClick="subFunction()" name="submit"><b>Submit</b></button>
-    </form>
+   <div class="demoparagraph">
+       <div class="row">
+            <form id="trxForm" name="trxForm" method="post" action="trx.php">
+                <div>
+                    <label for="trx">Amount </label>
+                    <input type="currency" id="trx" name="amount">
+                </div>
+                <div>
+                    <label for="type">Type </label>
+                    <select id="type" name="type">
+                        <option value="C">Credit</option>
+                        <option value="D">Debit</option>
+                    </select>
+                </div>
+                <button type="button" id="submit" onClick="subFunction()" name="submit"><b>Submit</b></button>
+            </form>
+       </div>
+   </div>
+    
     <?php 
         require_once "db.php";
 
@@ -141,6 +208,7 @@
 
     <div class="col-md-4">
         <div class="row">
+            <h3>Balance: <?= number_format($total, 2, ',', '.') ?></h3>
             <table class="blueTable" width="100%">
                 <thead>
                     <th>#</th>
@@ -173,7 +241,6 @@
         </div>
     </div>
 
-    <h3>Balance: <?= number_format($total, 2, ',', '.') ?></h3>
     <script type="text/javascript">
         function subFunction(){
             let data = {amount: document.getElementById("trx").value,  type: document.getElementById("type").value};
@@ -189,6 +256,8 @@
         function getTrx(){
 
         }
+        
+        
     </script>
 </body>
 </html>
